4fecef396a1de1a7b0c02fd63e9853e2fd3102f6
CRASHs4aint <samratkumarsahu228@gmail.com>
2025-08-01 00:27:51 +0530

    clear comment

--- app.js
+++ app.js
@@ -34,12 +34,6 @@ boxes.forEach((box) => {
 
 const checkWinner = () => {
   for (let pattern of winPatterns) {
-    // console.log(pattern[0], pattern[1], pattern[2]);
-    // console.log(
-    //   boxes[pattern[0]].innertext,
-    //   boxes[pattern[1]].innertext,
-    //   boxes[pattern[2]].innertext
-    // );
     let pos1Val = boxes[pattern[0]].innertext;
     let pos2Val = boxes[pattern[1]].innertext;
     let pos3Val = boxes[pattern[2]].innertext;
